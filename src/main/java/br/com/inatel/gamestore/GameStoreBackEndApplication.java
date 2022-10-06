728cd47f051a96186007e94976ecd2058db9cbc1
JuninhoCarvalho <juninho.maglioni@gmail.com>
2022-10-06 00:03:46 -0300

    :tada: Adicionando projeto

---
+++ src/main/java/br/com/inatel/gamestore/GameStoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.inatel.gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/inatel/gamestore/GameStoreBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inatel.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
